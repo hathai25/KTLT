043b3691d215c474e12efb5608e7d490acfb4092
hathai15 <74005327+hathai25@users.noreply.github.com>
2022-05-09 14:47:47 +0700

    Update bt1.c

--- TH1/bt1.c
+++ TH1/bt1.c
@@ -1,6 +1,8 @@
 # include <stdio.h>
 int main(){
     //Pham Huy Ha Thai - 20204784
+    printf("Ho Va Ten: Pham Huy Ha Thai");
+    printf("MSSV: 20204784 ");
     int x, y, z;
     int* ptr;
     printf("Enter three integers: ");
@@ -8,13 +10,12 @@ int main(){
     printf("\nThe three integers are:\n");
     ptr = &x;
     printf("x = %d\n", *ptr);
-    ptr = &y;
+    /*****************/
+    ptr = &y; //gan con tro toi dia chi y
     printf("y = %d\n", *ptr);
-    ptr = &z;
+    ptr = &z; //gan con tro toi dia chi z
     printf("z = %d\n", *ptr);
-    /*****************
-    # YOUR CODE HERE #
-    *****************/
+    /*****************/
     
     return 0;
 }
